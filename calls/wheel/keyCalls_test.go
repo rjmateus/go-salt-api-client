9f55b9e0e8fa190b691715e313343bf6a116057b
Ricardo Mateus <rmateus@suse.com>
2022-06-02 22:23:23 +0100

    test refactoring
    Signed-off-by: Ricardo Mateus <rmateus@suse.com>

--- calls/wheel/keyCalls_test.go
+++ calls/wheel/keyCalls_test.go
@@ -1,16 +1,14 @@
 package wheel
 
 import (
-	"github.com/rjmateus/go-salt-api-client/client"
 	"github.com/rjmateus/go-salt-api-client/testUtils"
 	"testing"
 )
 
 func TestKeyListAllSyncOK(t *testing.T) {
-	httpServer := testUtils.GetHttpServer()
-	htppClient := client.NewClient(httpServer.URL, "", "", "")
-
 	call := KeyListAllSync()
+	htppClient := testUtils.MockedSaltFunResponseClient(call.Fun)
+
 	if call.Fun != "key.list_all" {
 		t.Errorf("wrong fun name. got: %s, want: 5%s", call.Fun, "key.list_all")
 	}
--- testUtils/server.go
+++ testUtils/server.go
@@ -1,20 +1,36 @@
 package testUtils
 
 import (
+	"fmt"
+	"github.com/rjmateus/go-salt-api-client/client"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"path"
+	"runtime"
 )
 
-func GetHttpServer() *httptest.Server {
-	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		bytes, error := ioutil.ReadFile("all_keys_test.json")
-		if error != nil {
-			log.Fatal(error)
-		}
-		w.Write(bytes)
-	}))
+func getHttpServerResponseFile(fileName string, status int) *httptest.Server {
+	_, thisFileName, _, _ := runtime.Caller(1)
 
+	bytes, error := ioutil.ReadFile(path.Join(path.Dir(thisFileName), fileName))
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	return GetHttpServer(bytes, status)
+}
+
+func GetHttpServer(response []byte, status int) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(status)
+		w.Write(response)
+	}))
+}
+
+func MockedSaltFunResponseClient(funName string) client.SaltClient {
+	http := getHttpServerResponseFile(fmt.Sprintf("%s_reponse.json", funName), http.StatusOK)
+	htppClient := client.NewClient(http.URL, "", "", "")
+	return htppClient
 }
